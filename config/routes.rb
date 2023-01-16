08d68a865edb5dab96321da6a458324ef661a80c
estelle rambur <estelle.rambur@gmail.com>
2023-01-16 13:32:40 -0500

    add route index and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "projects#index"
+  root to: 'pages#home'
+  resources :projects, only: [:index, :show]
 end
