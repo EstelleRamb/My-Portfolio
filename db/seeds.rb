228089057c6e7599c2fd6d40dc9cd301dacb4c83
estelle rambur <estelle.rambur@gmail.com>
2023-01-16 13:42:47 -0500

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Project.create!(
+  name: 'Le théâtre de la Colinne',
+  genre: 'graphism',
+  description: "Le théâtre de la Colline est un théâtre National situé à l’Est Parisien.
+                Ce projet a été mon projet de diplôme de ma Licence Professionnelle.
+                J’ai réalisé la communication de la saison culturelle 2014-2015 de ce
+                théâtre. Ce qui comprend le programme, le dépliant, et les affiches.
+                Concept de la double lecture avec une production en bichromie et un
+                système de filtre qui masque et/ou dévoile des éléments imprimés.
+                Programme comprenant l'ensemble des spectacles de l'année",
+  photo_url: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+  photo_url1: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+  photo_url2: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+  photo_url3: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+  photo_url4: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+)
