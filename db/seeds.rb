bde4cc7c1935e1c3bdc8311181c4750776be3c1b
estelle rambur <estelle.rambur@gmail.com>
2023-01-17 18:37:59 -0500

    Add more photo in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,10 @@ Project.create!(
                 système de filtre qui masque et/ou dévoile des éléments imprimés.
                 Programme comprenant l'ensemble des spectacles de l'année",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896995/production/Portfolio/affiche-novembre_rqljgw.jpg',
-  photo_url1: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
-  photo_url2: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
-  photo_url3: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
-  photo_url4: 'https://static.wixstatic.com/media/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg/v1/fill/w_854,h_1160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/cd8c02_861ce683371e4aaab28dc888a8e922ab.jpg',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997960/production/Portfolio/programme-page-8_jffsjy.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997959/production/Portfolio/2_xnyxjo.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997960/production/Portfolio/Affiche-blanche_hxdsvm.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997959/production/Portfolio/Affiche-rouge_njxvxf.jpg',
 )
 
 Project.create!(
@@ -38,10 +38,10 @@ Project.create!(
                 des échanges avec d'autres utilisateurs de l'application.
                 Langages : Ruby on rails, HTML, SCSS, Javascript",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896832/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.06.39_u8qt3y.png',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998083/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.10.02_pyvabd.png',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998086/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.08.10_il6tbd.png',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998090/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.07.13_vs1kpt.png',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998089/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.07.43_vi0osp.png',
 )
 
 Project.create!(
@@ -52,10 +52,10 @@ Project.create!(
                 soit même des suspensions, décorations murales et autres.
                 Outils : Illustrator",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896975/production/Portfolio/magasin_hjopir.png',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998184/production/Portfolio/serie_logos_uzswjv.png',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998183/production/Portfolio/Carte_visite_recto_hsybnq.png',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998195/production/Portfolio/stikers_s4tsbx.png',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998197/production/Portfolio/carte_ge9gpt.png',
 )
 
 Project.create!(
@@ -64,10 +64,10 @@ Project.create!(
   description: "Création de contenu pour les réseaux sociaux du fleuriste Pourquoi pas fleurs.
                 Je propose chaque jours un nouvel arrangement floral avec de nouvelles harmonies colorées.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896953/production/Portfolio/03_gpsv2o.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673996959/production/Portfolio/IMG_1810_edited_fe2fjz.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673996958/production/Portfolio/IMG_1519_ot89wu.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673996958/production/Portfolio/IMG_1746_oybsty.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673996959/production/Portfolio/IMG_1528_w8pt6f.jpg',
 )
 
 Project.create!(
@@ -80,10 +80,10 @@ Project.create!(
                 photo montages en noir et blanc, sur la thématique : Entre mets et vins.
                 L’ensemble représente un bouteille de vin, grâce à des aplats rouges",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896977/production/Portfolio/Photo_finale_seqlxt.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997027/production/Portfolio/Screen_Shot_2015-04-29_at_13.23.24_bwtqff.png',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997027/production/Portfolio/Screen_Shot_2015-04-29_at_13.22.44_dm9ldw.png',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997027/production/Portfolio/Photo_finale_2_puetxc.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997026/production/Portfolio/Edited_Image_2015-7-1-17_43_9_zx3tpg.jpg',
 )
 
 Project.create!(
@@ -93,10 +93,10 @@ Project.create!(
                 anesthésiste de l'Institut Marie Curie.
                 Réalisation de l'Affiche, du Flyers et de la Couverture du livret explicatif.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673897993/production/Portfolio/Mise_en_situation_Affiche_lfdm0f.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997095/production/Portfolio/Affiche_igkp2v.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997096/production/Portfolio/Mise_en_situation_livret_eoffmw.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997093/production/Portfolio/Livret_couverture_orim3f.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997092/production/Portfolio/Flyer_o8qztc.jpg',
 )
 
 Project.create!(
@@ -107,10 +107,10 @@ Project.create!(
                 Recherches d'éléments graphique rappelant l'univers de l'architecture,
                 notamment le plan et ses niveaux de lecture.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896814/production/Portfolio/logo7_yzomx6.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997161/production/Portfolio/logo2_mckfat.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997161/production/Portfolio/logo6_gnozrk.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997161/production/Portfolio/logo5_o21rju.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997161/production/Portfolio/logo4_fbnkwx.jpg',
 )
 
 Project.create!(
@@ -121,10 +121,10 @@ Project.create!(
                 but d'attirer une clientèle jeune. L’utilisation d’un bleu glacial ainsi qu’un dessin d’un ours
                 polaire évoquent le froid scandinave mais créent aussi un univers frais, lumineux et sobre.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896860/production/Portfolio/Ensemble_support_mock_up_bmlyof.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997342/production/Portfolio/Cartes_visite_mock_up_crymaz.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997338/production/Portfolio/GOBELET_bjkuqy.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997338/production/Portfolio/dessous-de-verre_qx1x6s.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997338/production/Portfolio/sac_wzkvol.jpg',
 )
 
 Project.create!(
@@ -139,10 +139,10 @@ Project.create!(
                  du public par leur identification à L’usine Cadbury. J’ai créé deux logos, pris des photos du lieu
                  pour mettre en place une page Facebook comportant un logo et une photo de couverture.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896773/production/Portfolio/logo_final_vect-01_amhqql.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997441/production/Portfolio/couv_fb_02_ffgepe.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997441/production/Portfolio/couv_fb_01_jrift9.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997441/production/Portfolio/couv_fb_03_aw0mhh.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997474/production/Portfolio/logo_final_vect-02_jojmtx.jpg',
 )
 
 Project.create!(
@@ -150,10 +150,10 @@ Project.create!(
   genre: 'photo',
   description: " Série de photo de l'Usine Cadbury à /montréal",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673897998/production/Portfolio/IMG_9921_fhnw1h.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997501/production/Portfolio/IMG_9995_g2qv7t.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997501/production/Portfolio/IMG_9909_mmaumj.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997500/production/Portfolio/IMG_9928_e2vif6.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997500/production/Portfolio/IMG_0230_ajgdd4.jpg',
 )
 
 Project.create!(
@@ -162,10 +162,10 @@ Project.create!(
   description: "Conférence sur le thème de les pouvoirs humains sur les génômes. Réalisation de l'Affiche, du Flyers
                 et du livret explicatif.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896854/production/Portfolio/08_urban_poster_mockup_gwnehn.jpg',
-  photo_url1: '',
-  photo_url2: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997547/production/Portfolio/Affiche_JED_sysv02.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997542/production/Portfolio/Affiche_VECT2_betuy1.png',
   photo_url3: '',
-  photo_url4: '',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997544/production/Portfolio/LOGO_3_kgten0.jpg',
 )
 
 Project.create!(
@@ -175,10 +175,10 @@ Project.create!(
                 pour passer des nuits parfaites. Vous pouvez aussi mettre en location vos propres rêves.
                 Langages: Ruby on Rails, HTML, Javascript, SCSS",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896760/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.12.50_zxmwvm.png',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997658/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.13.29_eaustu.png',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997657/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.13.45_hdbkui.png',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997657/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.13.03_k4yyz5.png',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997656/production/Portfolio/Capture_d_e%CC%81cran_le_2023-01-16_a%CC%80_13.14.04_yc7q8b.png',
 )
 
 Project.create!(
@@ -190,8 +190,8 @@ Project.create!(
                 ressemblances entre ces deux êtres. Les brochures abordent ces similitudes sur trois
                 axes : la génétique, le greffe et le comportement.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896808/production/Portfolio/Affiche_abribus_wnnxlj.jpg',
-  photo_url1: '',
-  photo_url2: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997854/production/Portfolio/page2_iwvmzk.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673997854/production/Portfolio/brochure3_b2pmvo.jpg',
   photo_url3: '',
   photo_url4: '',
 )
@@ -205,9 +205,9 @@ Project.create!(
                 le logo officiel de McGill en deux déclinaisons. Un pour les panneaux publicitaire, les affiches
                 et un pour la papeterie et le site internet.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896872/production/Portfolio/mcgill_com_RGB_web_abdo06.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998252/production/Portfolio/maq_mcgill_baniere_V09_2_l1h1ob.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998251/production/Portfolio/maq_mcgill_papier_V09_aezisl.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998251/production/Portfolio/mcgill_com_h_RGB_web_umzmu2.jpg',
   photo_url4: '',
 )
 
@@ -217,10 +217,10 @@ Project.create!(
   description: "Rapport de stage réalisé à la suite d'un stage de 3 mois au sein du studio
                 Bicouac Studio à Montréal.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896901/production/Portfolio/IMG_0484_x8rtik.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998349/production/Portfolio/IMG_0470_r2fxxj.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998349/production/Portfolio/IMG_0478_wkhhs9.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998349/production/Portfolio/IMG_0483_ocolcg.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998349/production/Portfolio/IMG_0457_xuicuh.jpg',
 )
 
 Project.create!(
@@ -229,10 +229,10 @@ Project.create!(
   description: "Série d’affiches autour des critiques du film de Xavier Dolan « Mommy », qui a beaucoup fait parler.
                 J’ai exploité les critiques des magazines et j’ai créé un univers graphique autour de chacune.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896923/production/Portfolio/livre-fermer_iaaiie.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998409/production/Portfolio/livre1_essxwl.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998409/production/Portfolio/A9_n2dwea.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998409/production/Portfolio/IMG_0552_h8pg4m.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998409/production/Portfolio/A6_cd7c3c.jpg',
 )
 
 Project.create!(
@@ -240,10 +240,10 @@ Project.create!(
   genre: 'graphism',
   description: "Experimentation de l'isométrie sur Illustrator",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896984/production/Portfolio/summer-fruits_fskrql.png',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998471/production/Portfolio/Fruits_gtwit4.png',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998470/production/Portfolio/Citron_ananas_obsbr4.png',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998471/production/Portfolio/Summer_isometric_r0rmdn.png',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998470/production/Portfolio/Orange_Ananas_boayxs.png',
 )
 
 Project.create!(
@@ -255,10 +255,10 @@ Project.create!(
                 Exaltation, Nostalgie sont les 5 parfums de la gamme proposée. Une alternative chic et insolite
                 aux désormais trop classiques macarons.",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896938/production/Portfolio/ensemble_jxjato.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998519/production/Portfolio/exaltation_yuuhev.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998519/production/Portfolio/carte-recto_gcqz1f.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998519/production/Portfolio/bo%C3%AEtes_t0q9qz.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998519/production/Portfolio/bonheur_mz3qjb.jpg',
 )
 
 Project.create!(
@@ -268,9 +268,9 @@ Project.create!(
                 Le projet était de réaliser la communication visuelle d'un théâtre Parisien, comprenant les
                 affiches pour la saison annuelle ainsi que la programmation sous forme de livre et une brochure",
   photo_url: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673896886/production/Portfolio/m%C3%A9moire-gros-plan-pages_iisqpz.jpg',
-  photo_url1: '',
-  photo_url2: '',
-  photo_url3: '',
-  photo_url4: '',
+  photo_url1: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998574/production/Portfolio/m%C3%A9moire-double-page-_3_yvrzn3.jpg',
+  photo_url2: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998574/production/Portfolio/m%C3%A9moire-double-page-_4_dmpc4g.jpg',
+  photo_url3: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998574/production/Portfolio/m%C3%A9moire-double-page-_5_jxpamk.jpg',
+  photo_url4: 'https://res.cloudinary.com/dvw6upciw/image/upload/v1673998573/production/Portfolio/m%C3%A9moire-couverture_a5mvm1.jpg',
 )
 puts "all done"
