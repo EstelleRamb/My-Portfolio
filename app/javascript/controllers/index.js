2db12db092273d898d6364245b485ce78ab3720e
estelle rambur <estelle.rambur@gmail.com>
2023-01-19 14:36:32 -0500

    enlever icon.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,6 +9,3 @@ application.register("card", CardController)
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
-
-import IconController from "./icon_controller"
-application.register("icon", IconController)
